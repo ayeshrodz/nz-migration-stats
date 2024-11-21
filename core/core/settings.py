43b095af6a28c997baa8addad5534a8340cb5012
Ayesh Rodrigo <ayeshrodz@gmail.com>
2024-11-22 02:06:41 +1300

    Azure frendly conf

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'data_app',
     'crispy_forms',
     'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'core.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8000'
+]
 
 TEMPLATES = [
     {
